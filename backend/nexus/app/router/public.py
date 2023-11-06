7903eb10f9f128f5ac82286ac42e0c48ab5366aa
Shiva <shivajreddy@outlook.com>
2023-11-06 08:11:30 -0500

    /public routes for departments fixed

--- backend/nexus/app/router/public.py
+++ backend/nexus/app/router/public.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from app.database.database import eagle_data_coll
+
 """
 All public end points - visible to out of eagle
 """
@@ -10,3 +12,10 @@ router = APIRouter(prefix="/public")
 @router.get('/test')
 def test_public():
     return {"Public testing route works"}
+
+
+@router.get(path='/departments')
+def get_all_departments():
+    departments_doc = eagle_data_coll.find_one({"table_name": "departments"})
+    all_departments = departments_doc["departments_names"]
+    return all_departments
